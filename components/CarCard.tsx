022c32634f1262b869f9bb2d3cd651da57c1f6b6
Chirill Ciumeica <kirillchumeika@list.ru>
2024-09-06 20:49:41 +0300

    Card Details modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import { CarProps } from '@/types';
 
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
     car: CarProps;
@@ -74,6 +75,8 @@ const CarCard = ({ car }: CarCardProps) => {
                 />
             </div>
         </div>
+
+        <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
     </div>
   )
 }
